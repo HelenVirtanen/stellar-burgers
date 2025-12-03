713696b85931f9a6e0f1b343d620e651f8c120a3
Helen Virtanen <helenvirtanen28@gmail.com>
2025-12-03 18:13:59 +0300

    feat(burger-constructor.ts): add burger constructor slice, reducer, and add store data to burger-constructor

--- src/services/rootReducer.ts
+++ src/services/rootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { ingredientsSlice } from '../slices/ingredients';
+import { constructorSlice } from '../slices/burger-constructor';
 
 export const rootReducer = combineReducers({
-  ingredients: ingredientsSlice.reducer
+  ingredients: ingredientsSlice.reducer,
+  burgerConstructor: constructorSlice.reducer
 });
