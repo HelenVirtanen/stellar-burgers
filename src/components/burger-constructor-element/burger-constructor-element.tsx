f334312228486c735bff6475076aef591fa9a78b
Helen Virtanen <helenvirtanen28@gmail.com>
2025-12-04 21:03:50 +0300

    feat(burger-constructor-element.tsx): add ingredients change order and remove

--- src/components/burger-constructor-element/burger-constructor-element.tsx
+++ src/components/burger-constructor-element/burger-constructor-element.tsx
@@ -1,14 +1,27 @@
 import { FC, memo } from 'react';
 import { BurgerConstructorElementUI } from '@ui';
 import { BurgerConstructorElementProps } from './type';
+import {
+  changeIngredientsOrder,
+  removeIngredient
+} from '../../slices/burger-constructor';
+import { useDispatch } from '../../services/store';
 
 export const BurgerConstructorElement: FC<BurgerConstructorElementProps> = memo(
   ({ ingredient, index, totalItems }) => {
-    const handleMoveDown = () => {};
+    const dispatch = useDispatch();
 
-    const handleMoveUp = () => {};
+    const handleMoveDown = () => {
+      dispatch(changeIngredientsOrder({ from: index, to: ++index }));
+    };
 
-    const handleClose = () => {};
+    const handleMoveUp = () => {
+      dispatch(changeIngredientsOrder({ from: index, to: --index }));
+    };
+
+    const handleClose = () => {
+      dispatch(removeIngredient(+ingredient.id));
+    };
 
     return (
       <BurgerConstructorElementUI
