5700438d3cd03e3c55b7eff3d490b97341f87b9c
Helen Virtanen <helenvirtanen28@gmail.com>
2025-12-04 21:19:11 +0300

    feat(ingredient-details.tsx): add ingredients details by id from store

--- src/components/ingredient-details/ingredient-details.tsx
+++ src/components/ingredient-details/ingredient-details.tsx
@@ -1,10 +1,16 @@
 import { FC } from 'react';
 import { Preloader } from '../ui/preloader';
 import { IngredientDetailsUI } from '../ui/ingredient-details';
+import { useParams } from 'react-router-dom';
+import { useSelector } from '../../services/store';
+import { getIngredients } from '../../slices/ingredients';
 
 export const IngredientDetails: FC = () => {
-  /** TODO: взять переменную из стора */
-  const ingredientData = null;
+  /** DONE: взять переменную из стора */
+  const ingredients = useSelector(getIngredients);
+  const { id } = useParams<{ id: string }>();
+  const ingredientData =
+    ingredients.find((ingredient) => ingredient._id === id) ?? null;
 
   if (!ingredientData) {
     return <Preloader />;
