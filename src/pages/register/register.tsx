952234dd3a68ed8b9a85870e82ab969d37fe76b9
Helen Virtanen <helenvirtanen28@gmail.com>
2025-12-06 14:50:20 +0300

    feat(register.tsx): add dispatch registerUser

--- src/pages/register/register.tsx
+++ src/pages/register/register.tsx
@@ -1,13 +1,20 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { RegisterUI } from '@ui-pages';
+import { registerUser } from '../../slices/user';
+import { useDispatch } from '../../services/store';
+import { useNavigate } from 'react-router-dom';
 
 export const Register: FC = () => {
   const [userName, setUserName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(registerUser({ name: userName, email, password }));
+    navigate('/login');
   };
 
   return (
