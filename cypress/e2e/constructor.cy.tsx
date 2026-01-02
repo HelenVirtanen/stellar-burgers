c55854164b8eb3449d6c17faf2b2909e4aed8061
Helen Virtanen <helenvirtanen28@gmail.com>
2026-01-02 15:42:25 +0300

    test(constructor.cy.tsx): check adding ingredients to constructor

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,9 +1,16 @@
 describe('Интеграционные тесты для страницы конструктора ', function () {
-  it('Настроен перехват запроса на эндпоинт api/ingredients', function () {
+  beforeEach('Настроен перехват запроса на эндпоинт api/ingredients', () => {
     cy.intercept('GET', '**/api/ingredients', {
       fixture: 'ingredients.json'
     }).as('getIngredients');
+
     cy.visit('http://localhost:4000');
     cy.wait('@getIngredients');
+  });
+
+  it('Добавление булки, начинки и соуса в конструктор', () => {
+    cy.get('[data-testid="ingredients-buns"]').find('button').first().click();
+    cy.get('[data-testid="ingredients-mains"]').find('button').first().click();
+    cy.get('[data-testid="ingredients-sauces"]').find('button').first().click();
   });
 });
