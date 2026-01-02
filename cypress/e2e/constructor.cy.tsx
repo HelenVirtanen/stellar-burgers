c98c5818f72cabacd777a8001aca972c5a14e074
Helen Virtanen <helenvirtanen28@gmail.com>
2026-01-02 14:30:53 +0300

    test(constructor.cy.tsx): add intercept for GET api/ingredients

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,5 +1,9 @@
-describe('Проверяем доступность приложения', function () {
-  it('Сервис должен быть доступен по адресу localhost:4000', function () {
+describe('Интеграционные тесты для страницы конструктора ', function () {
+  it('Настроен перехват запроса на эндпоинт api/ingredients', function () {
+    cy.intercept('GET', '**/api/ingredients', {
+      fixture: 'ingredients.json'
+    }).as('getIngredients');
     cy.visit('http://localhost:4000');
+    cy.wait('@getIngredients');
   });
 });
